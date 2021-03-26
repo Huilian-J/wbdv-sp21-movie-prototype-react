3d1ab5162fdabc11d95ddf70e39d4a6879d3d305
Huilian-J <jiang.hui@northeastern.edu>
2021-03-25 23:50:22 -0400

    fetch movies

---
+++ src/components/details.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Details = () => {
+    return (
+        <div>
+            <h1>Details</h1>
+            <img src=""/>
+            <p>
+               Plot
+            </p>
+            <ul>
+                <li>Harrison Ford</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Details
