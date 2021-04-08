91f00d49c801a00cd8d95f0184492bb44b33fefe
yufeng-gao <77986637+yufeng-gao@users.noreply.github.com>
2021-04-08 13:43:16 -0400

    Update search-screen.js

--- src/components/search-screen.js
+++ src/components/search-screen.js
@@ -3,7 +3,11 @@ import { Link, useParams, useHistory } from "react-router-dom";
 import movieService from "../services/movie-service";
 
 const SearchScreen = () => {
+<<<<<<< Updated upstream
     const history = useHistory();
+=======
+    // const history = useHistory();
+>>>>>>> Stashed changes
     const { title, pageId } = useParams();
     const [searchTitle, setSearchTitle] = useState(title);
     const [popular, setPopular] = useState(true);
@@ -22,6 +26,10 @@ const SearchScreen = () => {
                 })
         } else {
             setPopular(false)
+<<<<<<< Updated upstream
+=======
+            // history.push(title);
+>>>>>>> Stashed changes
             if (pageId === undefined || pageId === "") {
                 movieService.findMoviesByTitle(title, 1)
                     .then((results) => {
@@ -38,7 +46,41 @@ const SearchScreen = () => {
     }
     return (
         <div>
+<<<<<<< Updated upstream
             <h2>Search Screen</h2>
+=======
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <img width="50" src={tmdb} />
+                <a className="navbar-brand" href="#">MovieDB Search</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    <button type="button" className="btn btn-primary">Sign In</button>
+                </div>
+            </nav>
+            {/*<table>*/}
+            {/*    <tbody>*/}
+            {/*    <tr>*/}
+            {/*        <td>*/}
+            {/*            <img width="50" src={tmdb} />*/}
+            {/*        </td>*/}
+            {/*        <td>*/}
+            {/*            MovieDB Search*/}
+            {/*        </td>*/}
+            {/*    </tr>*/}
+            {/*    </tbody>*/}
+            {/*</table>*/}
+            <p><img width="50" src={searchmovie} />Search Movie</p>
+>>>>>>> Stashed changes
             <input value={searchTitle}
                 onChange={(event) => {
                     setSearchTitle(event.target.value)
@@ -58,7 +100,10 @@ const SearchScreen = () => {
                     Search
                 </Link>
             }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
             {
                 popular && <h3>Popular Movies</h3>
             }
@@ -77,6 +122,7 @@ const SearchScreen = () => {
             </ul>
             {
                 (!popular && (Number(results.page) >= 1) && (Number(results.page) <= results.total_pages)) &&
+<<<<<<< Updated upstream
                 <div className="container">
                     <div className="row">
                         {
@@ -95,6 +141,24 @@ const SearchScreen = () => {
                             </Link>
                         }
                     </div>
+=======
+                <div className="row">
+                    {
+                        (Number(results.page) > 1) &&
+                        <Link to={`/search/${title}/page/${Number(results.page) - 1}`}
+                            className="btn btn-outline-primary col-2">
+                            &lt;&lt;
+                        </Link>
+                    }
+                    <div className="col-2">{results.page}</div>
+                    {
+                        (Number(results.page) < results.total_pages) &&
+                        <Link to={`/search/${title}/page/${Number(results.page) + 1}`}
+                            className="btn btn-outline-primary col-2">
+                            &gt;&gt;
+                            </Link>
+                    }
+>>>>>>> Stashed changes
                 </div>
             }
         </div>
